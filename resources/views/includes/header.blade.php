c22f49f51f148cd3465e99986efb162529e63853
mneacsu <mneacsu@intuitext.ro>
2017-03-30 18:45:30 +0300

    update page preturi

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,9 +3,9 @@
             <a href="{{ route('home') }}" title="Logo Gardinero" class="logo-header"><img src="frontend/assets/images/logo-gardinero-white.png" alt="Logo Gardinero"></a>
             <ul>
                 <li><a href="/servicii" title="Servicii">Servicii</a></li>
-                <li><a href="" title="Preturi">Preturi</a></li>
-                <li><a href="" title="De ce noi">De ce noi</a></li>
-                <li><a href="" title="Intrebari">Intrebari</a></li>
+                <li><a href="/preturi" title="Preturi">Preturi</a></li>
+                <li><a href="/de-ce-noi" title="De ce noi">De ce noi</a></li>
+                <li><a href="/intrebari" title="Intrebari">Intrebari</a></li>
             </ul>
             @if(Auth::user())
                 <div class="user-info-logged">Salut, <a href="{{ route('contul-meu.index') }}">{{ Auth::user()->firstname }}</a></div>
--- resources/views/pages/view.blade.php
+++ resources/views/pages/view.blade.php
@@ -14,81 +14,31 @@
         </div>
     </div>
     <div class="container">
+        {{--{!! $page->content !!}--}}
         <div class="row">
-            <div class="col-lg-1 col-sm-1 col-xs-3 services">
-                <h3>Intretinere gazon</h3>
-                <ul>
-                    <li><a href="">Tundere gazon</a></li>
-                    <li><a href="">Aerisire gazon</a></li>
-                    <li><a href="">Scarificare</a></li>
-                    <li><a href="">Gazonare</a></li>
-                    <li><a href="">Revigorare gazon</a></li>
-                    <li><a href="">Greblare</a></li>
-                    <li><a href="">Suprainsamantare</a></li>
-                </ul>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center page-info">
+                <h4>Curtea ta se află în cele mai bune mâini</h4>
+                <img src="frontend/assets/images/separator.png" alt="" class="separator-line">
+                <p>Mai jos puteti gasi lista de preturi standard insa in functie de numarul de comenzi<br>din zona dumneavoastra ne vom asigura ca vom oferi preturi mai mici.</p>
+                <img src="frontend/assets/images/separator.png" alt="" class="separator-line">
             </div>
-            <div class="col-lg-1 col-sm-1 col-xs-3 services">
-                <h3>Ierbicidare</h3>
-                <ul>
-                    <li><a href="">Ierbicidare burieni</a></li>
-                    <li><a href="">Aplicare ingrasaminte</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-1 col-sm-1 col-xs-3 services">
+        </div>
+
+        <div class="row">
+            <div class="col-lg-offset-1 col-lg-1 services text-center no-padd">
                 <h3>Toaletare copaci</h3>
-                <ul>
-                    <li><a href="">Taiare de formare a coroanei</a></li>
-                    <li><a href="">Taiere pentru regenerare a copacilor</a></li>
-                </ul>
+                <table cellpadding="0" cellspacing="0" border="0" class="prices-table" align="center">
+                    <tr>
+                        <td class="text-left">Tundere</td>
+                        <td valign="bottom" width="40%" class="dotted"><span></span></td>
+                        <td class="text-right">2 lei/mp</td>
+                    </tr>
+                </table>
             </div>
+
+
         </div>
-        <div class="row">
-            <div class="col-lg-1 col-sm-1 col-xs-3 services">
-                <h3>Insecticide</h3>
-                <ul>
-                    <li><a href="">Insecticide</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-1 col-sm-1 col-xs-3 services">
-                <h3>Plantare gazon Seminte</h3>
-                <ul>
-                    <li><a href="">Pregatire teren</a></li>
-                    <li><a href="">Insamantare gazon</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-1 col-sm-1 col-xs-3 services">
-                <h3>Plantare gazon rulou</h3>
-                <ul>
-                    <li><a href="">Pregatire teren</a></li>
-                    <li><a href="">Plantare gazon*</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-1 col-sm-1 col-xs-3 services">
-                <h3>Plantare copaci</h3>
-                <ul>
-                    <li><a href="">Pregatire teren</a></li>
-                    <li><a href="">Plantare copaci</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-1 col-sm-1 col-xs-3 services">
-                <h3>Amenajare curti</h3>
-                <ul>
-                    <li><a href="">Proiectare peisagistica</a></li>
-                    <li><a href="">Elemente decorative - roci, fantani, alei</a></li>
-                    <li><a href="">Proiectare si constructie zone legume bio</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-1 col-sm-1 col-xs-3 services">
-                <h3>Sisteme de irigatie</h3>
-                <ul>
-                    <li><a href="">Proiectare sisteme de irigatie</a></li>
-                    <li><a href="">Instalare sisteme de irigatie</a></li>
-                    <li><a href="">Instalare drenaje</a></li>
-                </ul>
-            </div>
-        </div>
+
     </div>
 
 @endsection
